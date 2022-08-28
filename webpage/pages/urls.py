edd3c0230756f086eb1cfcccb98f3ff4657ae2af
Tuomas Vaara <tuomas.vaara1999@gmail.com>
2022-08-28 14:26:53 +0300

    first view achieved

--- webpage/pages/urls.py
+++ webpage/pages/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 
-from . import views
+from .views import index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index') 
 ]
