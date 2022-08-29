f62134ff9c326b1f0eb28b295800830a62ecb0ca
Tuomas Vaara <tuomas.vaara1999@gmail.com>
2022-08-29 19:05:40 +0300

    setting up database

--- webpage/pages/models.py
+++ webpage/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+class Mail(models.Model):
+	content = models.TextField()
+
+class Message(models.Model):
+	source = models.ForeignKey(User, on_delete=models.CASCADE, related_name='source')
+	target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target')
+	content = models.TextField()
+	time = models.DateTimeField(auto_now_add=True)
