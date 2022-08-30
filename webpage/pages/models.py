e43656fc37e3c20de50a1e327cff128fd89cfcab
Tuomas Vaara <tuomas.vaara1999@gmail.com>
2022-08-30 10:01:00 +0300

    django tutorial part2

--- webpage/pages/models.py
+++ webpage/pages/models.py
@@ -1,12 +1,18 @@
+import datetime
+
 from django.db import models
-
-from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
-class Mail(models.Model):
-	content = models.TextField()
-
-class Message(models.Model):
-	source = models.ForeignKey(User, on_delete=models.CASCADE, related_name='source')
-	target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target')
-	content = models.TextField()
-	time = models.DateTimeField(auto_now_add=True)
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.question_text
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return self.choice_text
